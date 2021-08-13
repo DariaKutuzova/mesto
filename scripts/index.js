e853553770415f9dfea06711f9be954fdee54936
DariaKutuzova <kutuzovad94@gmail.com>
2021-08-13 17:51:58 +0300

    Style:Форматированы js и html

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,6 @@
 let openPopupButton = document.querySelector('.profile__edit-button');
 let closePopupButton = document.querySelector('.popup__close');
-let submitPopupButton = document.querySelector ('.popup__form-save');
+let submitPopupButton = document.querySelector('.popup__form-save');
 let popup = document.querySelector('.popup');
 let formElement = document.querySelector('.popup__container');
 let nameInput = document.querySelector('.popup__input-name');
@@ -19,15 +19,15 @@ function togglePopup() {
 openPopupButton.addEventListener('click', togglePopup);
 
 closePopupButton.addEventListener('click', togglePopup);
-document.querySelector('.popup__container').addEventListener('click', function(event) {event.stopPropagation();
+document.querySelector('.popup__container').addEventListener('click', function (event) {
+  event.stopPropagation();
 });
 
-function formSubmitHandler (evt) {
+function formSubmitHandler(evt) {
   evt.preventDefault();
   pageName.textContent = nameInput.value;
   pageJob.textContent = jobInput.value;
   togglePopup();
 }
 
-formElement.addEventListener ('submit', formSubmitHandler);
-
+formElement.addEventListener('submit', formSubmitHandler);
