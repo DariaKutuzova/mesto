a3094d156da6a8a5c9c1b3272cb0bd5805af78e2
DariaKutuzova <kutuzovad94@gmail.com>
2021-08-14 20:32:13 +0300

    Исправлены замечания ревью 1

--- scripts/index.js
+++ scripts/index.js
@@ -1,33 +1,42 @@
+// Объявляем переменные
 let openPopupButton = document.querySelector('.profile__edit-button');
 let closePopupButton = document.querySelector('.popup__close');
-let submitPopupButton = document.querySelector('.popup__form-save');
 let popup = document.querySelector('.popup');
-let formElement = document.querySelector('.popup__container');
-let nameInput = document.querySelector('.popup__input-name');
-let jobInput = document.querySelector('.popup__input-job');
+let formElement = document.querySelector('.popup__form');
+let nameInput = document.querySelector('.popup__input_value_name');
+let jobInput = document.querySelector('.popup__input_value_job');
 let pageName = document.querySelector('.profile__name');
 let pageJob = document.querySelector('.profile__description');
 
-function togglePopup() {
-  if (!popup.classList.contains('popup_opened')) {
+// Функция подтягивания значений со страницы в форму при открытии
+function valuePopup() {
+  if (popup.classList.contains('popup_opened')) {
     nameInput.value = pageName.textContent;
     jobInput.value = pageJob.textContent;
   }
+}
+
+// Функция открытия/закрытия попапа
+function openClose() {
   popup.classList.toggle('popup_opened');
 }
 
-openPopupButton.addEventListener('click', togglePopup);
+function togglePopup() {
+  openClose();
+  valuePopup()
+}
 
-closePopupButton.addEventListener('click', togglePopup);
-document.querySelector('.popup__container').addEventListener('click', function (event) {
-  event.stopPropagation();
-});
-
+// Функция присвоения значений из формы на страницу и закрытие попапа
 function formSubmitHandler(evt) {
   evt.preventDefault();
   pageName.textContent = nameInput.value;
   pageJob.textContent = jobInput.value;
-  togglePopup();
+  openClose();
 }
 
+// Вызываем функцию присвоения по клику на "Сохранить"
 formElement.addEventListener('submit', formSubmitHandler);
+// Вызываем функцию открытия попапа по клику
+openPopupButton.addEventListener('click', togglePopup);
+// Вызываем закрытие попапа по клику
+closePopupButton.addEventListener('click', openClose);
