1c9e867168925dcffc7765c1284894f70fd1fb5f
DariaKutuzova <kutuzovad94@gmail.com>
2021-10-13 01:59:48 +0300

    refactor:Убрать лишний агрумент в делите карточки

--- src/components/Card.js
+++ src/components/Card.js
@@ -60,7 +60,7 @@ export default class Card {
         this._element
             .querySelector('.element__trash')
             .addEventListener('click', () => {
-                this._deleteCard(this._element)
+                this._deleteCard()
             })
 
     }
